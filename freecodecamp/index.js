5a4882a7bbf25825983dc06fbf15098e81c8dfa5
trench009 <amennajam7@gmail.com>
2022-10-03 14:53:58 +0500

    add

---
+++ freecodecamp/index.js
@@ -0,0 +1,20 @@
+// today we're gonna know about the switch statement
+
+function casenoone(val){
+    let result = ""
+    switch(val){
+       case 1:
+        result="yes it works";
+        break;
+        case 2:
+            result="no it's not working";
+            break;
+        case 3:
+            result="yup it absolutely working";
+            break;
+            
+            
+    }
+    return result;
+}
+casenoone(2);
