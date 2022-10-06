c97a0bdffd24a8acc11e10d339f21d14e1c5ee71
trench009 <amennajam7@gmail.com>
2022-10-06 15:27:51 +0500

    add

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -165,10 +165,10 @@ for (var i = 0; i < 10; i += 2) {
   ourArray.push(i);
 }
 
-// Setup
+
 var myArray = [];
 
-// Only change code below this line.
+
 
 for (var i = 1; i < 10; i += 2) {
   myArray.push(i);
