53041b04627c2f0826abed6bcfd332fcfaba549b
trench009 <amennajam7@gmail.com>
2022-10-06 15:27:32 +0500

    add

--- freecodecamp/index.js
+++ freecodecamp/index.js
@@ -157,3 +157,22 @@ for(var i=0; i<6;i++){
 
     console.log(myArray);
 }
+
+// even numbers using for loop
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+for (var i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
+
+
+
