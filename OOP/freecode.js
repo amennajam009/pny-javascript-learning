3a1d1106030adeb369218d075e2f5b7df6d426e4
trench009 <amennajam7@gmail.com>
2022-10-07 11:11:26 +0500

    add

---
+++ OOP/freecode.js
@@ -0,0 +1,12 @@
+// OOP
+
+let dog ={
+name:"tommy",
+legs:4,
+
+sayLegs:function(){
+    return "how it can be possible that this dog has"+dog.legs+"lmao this sounds pathetic...!!"
+}    
+};
+
+dog.sayLegs();
