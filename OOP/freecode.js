0576162e29a9c86a1fb79d1a75c438bfb24392e8
trench009 <amennajam7@gmail.com>
2022-10-07 17:43:24 +0500

    add

--- OOP/freecode.js
+++ OOP/freecode.js
@@ -10,3 +10,82 @@ sayLegs:function(){
 };
 
 dog.sayLegs();
+
+
+
+
+
+
+function myphoneusers(val){
+ 
+    var myval='';
+    var lookup={
+  
+   "phone1":"amen",
+   "phone2":"harry bhai",
+   "phone3":"baba jani",
+   "phone4":"mama ",
+
+    };
+ myval=lookup[val];
+ return myval;
+ 
+}
+
+//COMPLEX OBJECTS
+
+var music={
+    "collecion":"my field",
+    "my film name":"rick nd morty",
+    "released_year":1993,
+    "form":[
+        "cd",
+        "movie",
+        "pop corn"
+    ],
+
+}
+
+//COMPLEX OBJECT USING DOT NOTATION
+
+
+var Myobjects={
+    "name":"amen",
+    "age":20,
+    "gender":"female",
+    "goals":"To be web developer",
+    "hobbies":{
+        "like to play guitar":"sia song",
+        "love to sing":"only in bathroom XD",
+        "sometimes love to play video games too":"GTA 5 , STAY,FORTNITE"
+    },
+    "LAST":"ONE",
+}
+
+var otheroneobject=Myobjects.name.age.gender.goals.hobbies["like to play guitar"];
+console.log(otheroneobject);
+
+
+//ASSESSING NESTED ARRAY
+
+
+const Myplants=[{
+    Fruits:{
+        "food":"borgor",
+        "drink":"juice",
+        "softdrinks":"coke" 
+    },
+    Juice:"grapes",
+    APPLE:"juice",
+    Banana:"yellow",
+},
+
+{
+    Colors:"yellow",
+    color:"green",
+    color:"blue",
+    color:"orange",
+}
+];
+
+Myplants[0].Fruits[1];
